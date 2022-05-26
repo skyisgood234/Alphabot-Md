a8635520e6a7d192bfa9a38397854bba454737c3
Sky4You <102728169+skyisgood234@users.noreply.github.com>
2022-05-26 09:26:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,26 +13,26 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read pesan / message
-//global.autorecording = true //status auto merekam ( auto record )
+//global.autorecording = false //status auto merekam ( auto record )
 //global.autoketik = false //status auto mengetik (auto typing)
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "sky4you" //namabot kalian
+global.ownername= "Skyller OFC" //nama kalian
+global.myweb ="https://instagram.com/sky4you" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UCRdVtigRg90kQ6Xs1q655vw" //bebas asal jan hapus
+global.github = "https://instagram.com/sky4you" //bebas
+global.email = "Skyllerofc9@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.ownernomer = "79615696897" // nomor wa kalian
+global.ownernomerr = "+79615696897" //nmr wa kalian
+global.thumbnail = "./image/lol_1.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi_1.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["79615696897","79615696897","6285817627091"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Sky4You' //sticker wm ubah
+global.author = 'But You4Me?' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
